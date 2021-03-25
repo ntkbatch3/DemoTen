c4c6e1b9c634eb46f11fe8804379a14ec76ec935
noort <noort@10.1.10.35>
2021-03-25 18:44:11 -0400

    m

--- src/demoabc/DemoTen.java
+++ src/demoabc/DemoTen.java
@@ -5,23 +5,11 @@ public class DemoTen {
 	public static void main(String[] args) {
 		
 
-		int aaaa = 101;
-		int bbbb = 11;
+		int a = 101;
+		int b = 11;
 		
-		System.out.println(aaaa+bbbb);
+		System.out.println(a+b);
 	}
 
-	
-	public static void add() {
-		System.out.println(11+22);
-	}
-	
-	public static void minus() {
-		System.out.println(11-1);
-	}
-	
-	
-	public static void div() {
-		System.out.println(2/1);
-	}
+ 
 }
